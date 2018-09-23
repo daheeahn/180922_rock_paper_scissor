68d11fe01b453782469b88f0193bbf3c179851b7
daheeahn <deg9810@likelion.org>
2018-09-23 16:07:36 +0900

    first commit

---
+++ app/src/main/java/org/techtown/rock_paper_scissors/MainActivity.java
@@ -0,0 +1,137 @@
+package org.techtown.rock_paper_scissors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.com_rock) ImageView com_rock;
+    @BindView(R.id.com_paper) ImageView com_paper;
+    @BindView(R.id.com_scissor) ImageView com_scissor;
+
+    @BindView(R.id.user_rock) ImageView user_rock;
+    @BindView(R.id.user_paper) ImageView user_paper;
+    @BindView(R.id.user_scissor) ImageView user_scissor;
+
+    @BindView(R.id.rock) ImageView rock;
+    @BindView(R.id.paper) ImageView paper;
+    @BindView(R.id.scissor) ImageView scissor;
+
+    @BindView(R.id.result) TextView result;
+    @BindView(R.id.text) TextView text;
+    @BindView(R.id.reset) Button reset;
+
+
+    int current_user;
+    int randomNum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        init_com();
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
+            }
+        });
+
+
+        rock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_rock.setVisibility(View.VISIBLE);
+                user_paper.setVisibility(View.INVISIBLE);
+                user_scissor.setVisibility(View.INVISIBLE);
+
+                current_user = 0;
+                result(randomNum, current_user);
+            }
+        });
+
+        paper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_rock.setVisibility(View.INVISIBLE);
+                user_paper.setVisibility(View.VISIBLE);
+                user_scissor.setVisibility(View.INVISIBLE);
+
+                current_user = 1;
+                result(randomNum, current_user);
+            }
+        });
+
+        scissor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_rock.setVisibility(View.INVISIBLE);
+                user_paper.setVisibility(View.INVISIBLE);
+                user_scissor.setVisibility(View.VISIBLE);
+
+                current_user = 2;
+                result(randomNum, current_user);
+            }
+        });
+    }
+
+    public void result(int randomNum, int current_user) {
+        int[][] indexArray = {
+                {0, 1, -1},
+                {-1, 0, 1},
+                {1, -1, 0}
+        };
+        //0 : 비김, 1 : user가 이김, -1 : user가 짐
+
+        if (indexArray[randomNum][current_user] == 0)
+            result.setText("비겼어요");
+        else if (indexArray[randomNum][current_user] == 1)
+            result.setText("이겼어요");
+        else if (indexArray[randomNum][current_user] == -1)
+            result.setText("졌어요");
+
+    }
+
+    public void init_com() {
+        Random r = new Random();
+        randomNum = r.nextInt(3);
+
+        if (randomNum == 0) {
+            com_rock.setVisibility(View.VISIBLE);
+            com_paper.setVisibility(View.INVISIBLE);
+            com_scissor.setVisibility(View.INVISIBLE);
+        } else if (randomNum == 1) {
+            com_rock.setVisibility(View.INVISIBLE);
+            com_paper.setVisibility(View.VISIBLE);
+            com_scissor.setVisibility(View.INVISIBLE);
+        } else {
+            com_rock.setVisibility(View.INVISIBLE);
+            com_paper.setVisibility(View.INVISIBLE);
+            com_scissor.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void reset() {
+        init_com();
+        text.setVisibility(View.VISIBLE);
+        user_rock.setVisibility(View.INVISIBLE);
+        user_paper.setVisibility(View.INVISIBLE);
+        user_scissor.setVisibility(View.INVISIBLE);
+        result.setText("선택하세요↓");
+    }
+
+}
